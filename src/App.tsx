7c69835188f3235bf191574d8555f09c2c676fc0
Sunny Kumar <goodluck14115@gmail.com>
2024-01-12 12:36:03 +0530

    Install tailwind CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1 className=" text-gray-700 text-4xl">Hello World</h1>
     </div>
   );
 }
